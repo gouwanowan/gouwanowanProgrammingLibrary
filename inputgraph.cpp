26ac2bb17c5ac4d4d0743519e0e1ee95f3ef8d47
Sma <gouwanowan@gmail.com>
2025-07-08 18:22:38 +0900

    Add WeightedGraphInput

--- inputgraph.cpp
+++ inputgraph.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cstdint>
 
 using namespace std ;
 
@@ -27,8 +28,29 @@ vector<vector<int64_t>> graphInput(int Nodes, int Edges, bool Undirected, bool O
     return list ;
 }
 
+vector<vector<pair<int64_t,int64_t>>> WeightedGraphInput(int Nodes, int Edges, bool Undirected, bool OneIndexed){
+    vector<vector<pair<int64_t,int64_t>>> list(Nodes, vector<pair<int64_t,int64_t>>(0)) ;
+    int StartIndex = 0 ;
+    if (OneIndexed)
+    {
+        StartIndex = 1 ;
+    }
+    for (int i = 0; i < Edges; i++)
+    {
+        int64_t a,b,c ;
+        cin >> a >> b >> c ;
+        list.at(a-StartIndex).push_back(make_pair(b-StartIndex,c)) ;
+        if (Undirected)
+        {
+            list.at(b-StartIndex).push_back(make_pair(a-StartIndex,c)) ;
+        }
+    }
+    return list ;
+}
+
 int main(){
     int n,m;
     cin >> n >> m ;
     vector<vector<int64_t>> list = graphInput(n,m,true,true) ;
+    //vector<vector<int64_t>> list = WeightedGraphInput(n,m,true,true) ;
 }
